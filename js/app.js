fd2d4a1ff24c3cb304e05c2fba483cb7d0537436
joshuanikolas <nikolassnchez@icloud.com>
2024-08-09 20:03:31 -0500

    currently adding javascript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,64 @@
+const questions = [
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Moment 4 Life", "Dear Old Nicki", "Starships", "Your Love"]
+        correct: 0
+         }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Red Ruby Da Sleeze", "Sir", "Press Play", "Good Form"]
+        correct: 0
+         }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Roman's Revenge", "Pound The Alarm", "Chun-Li", "Super Bass"]
+        correct: 2
+        }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["FTCU", "Pink Friday Girls", "Save Me", "High School"]
+        correct: 1
+        }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["I'm The Best", "Seeing Green", "Roman's Revenge", "Only"]
+        correct: 2
+        }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Barbie World", "Barbie Dreams", "Barbie Dangerous", "Barbie Tingz"]
+        correct: 0
+        }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Moment 4 Life", "Super Bass", "Fallin 4 U", "Anaconda"]
+        correct: 1
 
+    }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Did It On'em", "Whole Lotta Money", "Beeze In The Trap", "Barbie Dangerous"]
+        correct: 3
+    }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["Big Difference", "Pink Birthday", "Starships", "Fractions"]
+        correct: 2 
+    }
+    {
+        questions: "Play the song and guess which choice of a song name is the correct answer below:",
+        answers: ["All Things Go", "Last Time I Saw You", "The Crying Game", "Save Me"]
+        correct: 1
+    }
+    {
+        questions: "What is the name of Nicki Minaj's first album?",
+        answers: ["Queen", "The PinkPrint", "Pink Friday", "Pink Friday: Roman Reloaded"]
+        correct: 2
+    }
+]
+
+
+
+function displayQuestion() {
+    const questionElement = document.getElementById("questions");
+}
